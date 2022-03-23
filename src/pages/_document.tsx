2137194a8a3417972a75718fae138acf9ad750f0
Raellopes368 <raellopes368@gmail.com>
2022-03-23 08:01:44 -0300

    configura documento principal do next para importaçao das fontes do Google Fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,27 @@
-import Document from 'next/document';
+import Document, { Head, NextScript, Html, Main } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Ignite Blog</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="*"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
