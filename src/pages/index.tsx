84595de2e240e9e6b852d72e745e4e92e406fa05
Raellopes368 <raellopes368@gmail.com>
2022-03-24 07:45:26 -0300

    Retorno padrao para funçao Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,9 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return <h1>Hello world</h1>;
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
