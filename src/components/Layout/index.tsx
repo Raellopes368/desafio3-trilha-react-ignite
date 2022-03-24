3833954d23fa8a30abd6a3c5c09b16f1fbdff34d
Raellopes368 <raellopes368@gmail.com>
2022-03-24 07:44:07 -0300

    Cria layout para envolver todos os componentes

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,14 @@
+import Header from '../Header';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+}
